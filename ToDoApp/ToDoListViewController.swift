7ec375879353a54d89e5d514ce9200cd58189f9a
Ario Nugroho <gamerzdope@gmail.com>
2019-07-28 11:28:57 +0700

    add new alert button and action

--- ToDoApp/ToDoListViewController.swift
+++ ToDoApp/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["buy apple", "buy mango", "buy pen"]
+    var itemArray = ["buy apple", "buy mango", "buy pen"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,6 +57,40 @@ class ToDoListViewController: UITableViewController {
         }
         
     }
+    
+    //MARK: - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new to do item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            
+            // what will happen one the user clicks the Add Item button on our UIAlert
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = " Create new item"
+            textField = alertTextField
+            
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
     
 }
 
